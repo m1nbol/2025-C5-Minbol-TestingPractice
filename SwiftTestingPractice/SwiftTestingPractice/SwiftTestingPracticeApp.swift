c8e2eb2089c249c3bfa5c2272b7f0b9ec1a0020f
BoMin <bmlee@postech.ac.kr>
2025-08-14 17:00:46 +0900

    Feat: Create Xcode Project

---
+++ SwiftTestingPractice/SwiftTestingPractice/SwiftTestingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestingPracticeApp.swift
+//  SwiftTestingPractice
+//
+//  Created by BoMin Lee on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
