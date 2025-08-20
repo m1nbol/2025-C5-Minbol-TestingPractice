b9170e530312ea0ff0e6c44be49b3003afed720d
BoMin <bmlee@postech.ac.kr>
2025-08-20 17:10:09 +0900

    Feat: UITestStub 설정

--- ExchangeRateTesting/ExchangeRateTesting/App/AppDI.swift
+++ ExchangeRateTesting/ExchangeRateTesting/App/AppDI.swift
@@ -22,9 +22,24 @@ final class AppDI: ObservableObject {
         EXConfig.headerName = AppConfig.headerName
         EXConfig.authStyle  = .header
 
-        let provider = MoyaProvider<ExchangeRateAPI>(
-            plugins: [NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))]
-        )
+        let isUITestStub = ProcessInfo.processInfo.environment["UITESTS_STUB"] == "1"
+
+        let provider: MoyaProvider<ExchangeRateAPI> = {
+            if isUITestStub {
+                // 즉시 스텁: ExchangeRateAPI.sampleData 사용
+                return MoyaProvider<ExchangeRateAPI>(
+                    stubClosure: MoyaProvider.immediatelyStub
+                )
+            } else {
+                return MoyaProvider<ExchangeRateAPI>(
+                    plugins: [NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))]
+                )
+            }
+        }()
+        
+//        let provider = MoyaProvider<ExchangeRateAPI>(
+//            plugins: [NetworkLoggerPlugin(configuration: .init(logOptions: .verbose))]
+//        )
 
         let cache   = RateCache(ttl: 60)
         let repo    = ExchangeRateRepositoryImpl(provider: provider, cache: cache)
